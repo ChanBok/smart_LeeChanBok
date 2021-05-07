05e7d6877edede6eac99488ceb08560c1e858db9
ezen <ezen@DESKTOP-O03MT84>
2021-05-07 14:01:55 +0900

    Github Test 20210507

---
+++ smart_git_이찬복/src/smart_git_이찬복/Smart01.java
@@ -0,0 +1,9 @@
+package smart_git_ภฬย๙บน;
+
+public class Smart01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow Java^^");
+	}
+
+}
---
+++ smart_git_이찬복/src/smart_git_이찬복/Smart02.java
@@ -0,0 +1,9 @@
+package smart_git_ภฬย๙บน;
+
+public class Smart02 {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow students^^");
+	}
+
+}
